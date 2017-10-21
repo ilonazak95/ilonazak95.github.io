c46db1d973d72cc1db30df6fb0ab26691c61413a
Ilona Żak <ilona_zak@interia.pl>
2017-10-21 10:48:21 +0200

    typy danych

---
+++ javascript/js/datatypes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/*typ liczbowy */ 
+var premia = 1000;
+var wyplata = 5000;
+
+console.log(wyplata + premia);
+
+/* Typ- łancuch znaków- string */ 
+
+var premiaStr = "2500";
+var wyplataStr= "5000";
+
+console.log(wyplataStr + premiaStr);
+
+/* + -operator konkatenacji, sluzy do łaczenia string'ow, laczy je w jeden tekst */
+
+/* Typ logiczny */
+
+var czyJestSmog = true;
+console.log(czyJestSmog);
+
+/*Typy specjalne*/
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
