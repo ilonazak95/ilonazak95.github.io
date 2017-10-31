56cbb5753038a13c58db99cc0cec4625b823037d
Ilona Żak <ilona_zak@interia.pl>
2017-10-31 17:16:34 +0100

    Ajax- zad. domowe- pobieranie danych programisty

---
+++ 1-ajax-pobierz-dane-programisty/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+$(function () { 
+    
+    $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",function(data) { 
+    
+        $("#przycisk").click(function(){
+            console.log(data);
+            $("<div></div>").attr("id","dane-programisty").appendTo($("body"));
+        
+            $("#dane-programisty").text("Imię : " + data.imie + ", " +
+                                "Nazwisko : " + data.nazwisko + ", " + 
+                                "Zawód : " + data.zawod + ", " + 
+                                "Firma : " + data.firma);
+        });
+ 
+    });
+    
+});
