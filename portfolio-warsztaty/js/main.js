f498b6f757f8f337fd6e085eec10b8d8eb839570
Ilona Żak <ilona_zak@interia.pl>
2017-11-01 18:31:44 +0100

    portfolio- dodany smooth scroll, strona przewija sie do wskazanego elem. z menu

---
+++ portfolio-warsztaty/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(function () {
+    $("a[href]").click(function() {
+        
+        $("html,body").animate({
+            scrollTop: $(this.hash).offset().top
+        },700);
+    })
+
+});
