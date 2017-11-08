6669a64d4cb4640dddd5e7c3aaa0f42cbfbdc29a
Ilona Żak <ilona_zak@interia.pl>
2017-11-08 13:04:15 +0100

    dodane sekcje,ostylowane; brak dokonczonej responsywnosci

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,11 +1,22 @@
 'use strict';
-
 $(function () {
+//    scroll menu color change
+    if ($(window).scrollTop() >= 50) {
+            $('#main-nav').addClass('scroll');
+    }
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 50) {
+            $('#main-nav').addClass('scroll');
+            $('#main-nav').css({'transition':'all 0.5s'})
+        }else {
+            $('#main-nav').removeClass('scroll');
+        }
+    });
+//    smooth scroll to section
     $("a[href]").click(function() {
         
         $("html,body").animate({
             scrollTop: $(this.hash).offset().top
         },700);
     })
-
 });
