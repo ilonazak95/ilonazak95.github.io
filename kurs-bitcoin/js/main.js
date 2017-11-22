0db32e9be50f99cb48d329e883bda6453385e814
Ilona Żak <ilona_zak@interia.pl>
2017-11-22 16:21:04 +0100

    poprawiona responsywność

--- kurs-bitcoin/js/main.js
+++ kurs-bitcoin/js/main.js
@@ -1,41 +1,58 @@
 $(document).ready(function () {
-    $("#pobierz-dane").click(function () {
-        $.getJSON("https://blockchain.info/pl/ticker", function (data) {
-            console.log(data);
-            $("#kupno").text(data.PLN.buy);
-            $("#sprzedaz").text(data.PLN.sell);
+//    get json
+    $.getJSON("https://blockchain.info/pl/ticker", function (data) {
+        console.log(data);
+        $("#kupno").text(data.PLN.buy);
+        $("#sprzedaz").text(data.PLN.sell);
+    })
 
-            kupno(data.PLN.buy, data.PLN.last);
-            sprzedaz(data.PLN.sell, data.PLN.last);
-        })
-    });
+//    ajax
+    $("#pobierz-dane").click(function() {
+        $.ajax({
+            url: "https://blockchain.info/pl/ticker",
+            dataType: "json",
+            cache: false,
+            success: function (obj) {
+
+                $('#kupno').data(obj.PLN.buy);
+                $("#sprzedaz").text(obj.PLN.sell);
+
+                aktualizacjaKupna(obj.PLN.buy);
+                aktualizacjaSprzedazy(obj.PLN.sell);
+                    
+            }
+        });
+
+
+    })
+
+    function aktualizacjaKupna(normalneKupno, noweKupno) {
+        if (normalneKupno < noweKupno) {
+            $('#kupno').append('<i class="fa fa-arrow-up" aria-hidden="true"></i>');
+            $(".fa-arrow-up").addClass("wzrost-kupna");
+        } else if (normalneKupno > noweKupno) {
+            $('#kupno').append('<i class="fa fa-arrow-up" aria-hidden="true"></i>');
+            $(".fa-arrow-up").addClass("wzrost-kupna");
+        } else {
+            $('#kupno').append('<i class="fa fa-minus" aria-hidden="true"></i>')
+            $(".fa-minus").addClass("stala-wartosc");
+        }
+
+    }
+
+
+    function aktualizacjaSprzedazy(normalnaSprzedaz, nowaSprzedaz) {
+        if (normalnaSprzedaz < nowaSprzedaz) {
+            $('#sprzedaz').append('<i class="fa fa-arrow-up" aria-hidden="true"></i>');
+            $(".fa-arrow-up").addClass("wzrost-kupna");
+        } else if (normalnaSprzedaz > nowaSprzedaz) {
+            $('#sprzedaz').append('<i class="fa fa-arrow-down" aria-hidden="true"></i>');
+            $(".fa-arrow-down").addClass("spadek-kupna");
+        } else {
+            $('#sprzedaz').append('<i class="fa fa-minus" aria-hidden="true"></i>')
+            $(".fa-minus").addClass("stala-wartosc");
+        }
+    }
+
+
 });
-
-
-function kupno(buy, last) {
-    if (buy > last) {
-        $('#kupno').append('<i class="fa fa-arrow-up" aria-hidden="true"></i>')
-        $(".fa-arrow-up").addClass("wzrost-kupna")
-    } else if (buy < last) {
-        $('#kupno').append('<i class="fa fa-arrow-down" aria-hidden="true"></i>')
-        $(".fa-arrow-down").addClass("spadek-kupna")
-    } else {
-        console.log('stala');
-        $('#kupno').append('<i class="fa fa-minus" aria-hidden="true"></i>')
-        $(".fa-minus").addClass("stala-wartosc");
-    }
-};
-
-function sprzedaz(sell, last) {
-    if (sell > last) {
-        $('#sprzedaz').append('<i class="fa fa-arrow-up" aria-hidden="true"></i>')
-        $(".fa-arrow-up").addClass("wzrost-kupna")
-    } else if (sell < last) {
-        $('#sprzedaz').append('<i class="fa fa-arrow-down" aria-hidden="true"></i>')
-        $(".fa-arrow-down").addClass("spadek-kupna")
-    } else {
-        $('#sprzedaz').append('<i class="fa fa-minus" aria-hidden="true"></i>')
-        $(".fa-minus").addClass("stala-wartosc");
-    }
-};
-
